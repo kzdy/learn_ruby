0a6a59a26a74bd38c0438d124c19f6f30492c892
Konrad DeYoung <konrad.deyoung@gmail.com>
2017-01-11 18:10:53 -0800

    calculator.rb and test completed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -6,6 +6,41 @@ def subtract(num3,num4)
   num3 - num4
 end
 
-def sum array
-  array = 0
+def sum(array)
+  final_sum = 0
+  array.each do |variable|
+    final_sum = final_sum + variable
+end
+final_sum
+end
+
+def multiply(array2)
+  final_product = 1
+  array2.each do |variable2|
+    final_product = final_product * variable2
+end
+final_product
+end
+
+def power(one,two)
+  answer = one ** two
+  answer
+end
+
+def factorial(number)
+  if number == 0
+    1
+  else
+    first = number
+    array5 = []
+     while first >= 1
+       array5.push first
+       first = first - 1
+     end
+     final = 1
+     array5.each do |var|
+       final = final * var
+     end
+     final
+  end
 end
